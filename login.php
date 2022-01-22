bfc2464bab0ba0e7793ea65dfb0306a9f11bc632
saifudinzezy <saifudin.zezy7@gmail.com>
2022-01-22 10:06:52 +0700

    add placeholder

--- login.php
+++ login.php
@@ -58,9 +58,9 @@ if(isset($_POST['login']))
   <img class="mb-4" src="assets/login.jpg" alt="" width="72" height="72">
   <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
   <label for="email" class="sr-only">email</label>
-  <input type="email" id="email" class="form-control" placeholder="email" name="email" required autofocus>
+  <input type="email" id="email" class="form-control" placeholder="email" name="email" required autofocus value="admin@smkmuhul.com">
   <label for="password" class="sr-only">Password</label>
-  <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
+  <input type="password" id="password" class="form-control" placeholder="Password" name="password" required value="admin">
   <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Sign in</button>
 </form>
 </body>
