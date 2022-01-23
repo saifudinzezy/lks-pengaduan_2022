5ffd7e77748578cec346fdee6ab5fb729e82db64
saifudinzezy <saifudin.zezy7@gmail.com>
2022-01-23 14:45:12 +0700

    edit logouout.php

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-if(! isset($_SESSION['is_login']))
+if(!isset($_SESSION['is_login']))
 {
   header('location:login.php');
 }
--- logout.php
+++ logout.php
@@ -2,7 +2,5 @@
 session_start();
 session_unset();
 session_destroy();
-setcookie('username', '', 0, '/');
-setcookie('nama', '', 0, '/');
 header('location:login.php');
 ?>
